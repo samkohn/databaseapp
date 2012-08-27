df33f6190627b82f9143fe2c780d18226c0c9689
Sam <therunner05@gmail.com>
2012-08-27 17:59:12 -0400

    Cleaned up script.js

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,92 +1,30 @@
-$('document').ready(function()
-{
-	
-	/*
-	$.post('http://localhost:27080/_connect','server=localhost:27017',function(returned)
-	{
-		alert('connect returned: ' + returned)
-	});
-	
-	var found = $.get('localhost:27080/test/admin/_find','',function(returned)
-	{
-		alert('find returned: ' + returned);
-	});
-	
-	alert(found);
-	*/
-	/*
-	var getNewPerson = function(inFirstName, inLastName)
-	{
-		var person = 
-		{
-			"firstName" : inFirstName,
-			"lastName" : inLastName
-		};
-		return person;
-	}
-	
-	var getPersonDiv = function(inPerson)
-	{ 
-		$toReturn = $('<div/>').addClass('person');
-		$toReturn.html("First Name: " + inPerson.firstName + "<br/>Last Name: " + inPerson.lastName);
-		return $toReturn;
-	}
-	
-	var person = getNewPerson("Sam", "Kohn");
-	
-	
-	$main.append(getPersonDiv(person));
-	for(var i=0; i < 5; i++)
-	{
-		$main.append($('<span class="button">Click Me!</span>'));
-	}
-	$('.button').click(function() {alert("Clicked!")});
-	
-	*/
-	$main = $('#main');
-	
-	$people = $('.person');
-	$people.click(function() {
-	  console.log($(this).html());
-	});
+$('document').ready(function() {
 
-	$('#addButton').click(function() {
-	    console.log('clicked');
-	    $('#inputForm').attr('action', '/addedname')
-	    $('#inputForm').attr('method', 'POST');
-	    $('#inputForm').submit();
-	});
-	$('#queryButton').click(function() {
-	    console.log('clicked');
-	    $('#inputForm').attr('action', '/queried')
-	    $('#inputForm').attr('method', 'GET');
-	    $('#inputForm').submit();
-	});
-	$('#removeButton').click(function() {
-	    console.log('clicked');
-	    $('#inputForm').attr('action', '/removed')
-	    $('#inputForm').attr('method', 'POST');
-	    $('#inputForm').submit();
-	});
+  $people = $('.person');
+  $people.click(function() {
+    console.log($(this).html());
+  });
 
-/*
-	$main.click(function()
-	{
-		if(confirm("Change text?"))
-		{
-			$main.html("Changed Text!");
-		}
-		else
-		{
-			alert("Did not change text!");
-		}
-	});
-	
-/*
-	for(var i=0; i < 10; i++)
-	{
-	$main.append($('<div/>').addClass('small'));
-	$('.small').html("This is a small div ... but not any more!");
-	}
-*/
+  // Handlers for submitting the form
+  $('#addButton').click(function() {
+    console.log('clicked');
+    $('#inputForm').attr('action', '/addedname')
+    $('#inputForm').attr('method', 'POST');
+    $('#inputForm').submit();
+  });
+
+  $('#queryButton').click(function() {
+    console.log('clicked');
+    $('#inputForm').attr('action', '/queried')
+    $('#inputForm').attr('method', 'GET');
+    $('#inputForm').submit();
+  });
+
+  $('#removeButton').click(function() {
+    console.log('clicked');
+    $('#inputForm').attr('action', '/removed')
+    $('#inputForm').attr('method', 'POST');
+    $('#inputForm').submit();
+  });
+
 });
